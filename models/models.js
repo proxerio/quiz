b56c48a317fa6b2406cc7110c5bbe2006b82d18f
proxerio <proxerio@hotmail.com>
2015-06-18 17:38:21 -0300

    Autoload e despliegue en Heroku

--- models/models.js
+++ models/models.js
@@ -1,19 +1,19 @@
 var path = require('path');
 
-// Postgres DATABASE_URL = postgres://user:passwd@host:port/database
-// SQLite   DATABASE_URL = sqlite://:@:/
-var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\:(.*)/);
-var DB_name		= (url[6]||null);
-var user		= (url[2]||null);
-var pwd 		= (url[3]||null);
-var protocol 	= (url[1]||null);
-var dialect 	= (url[1]||null);
-var port 		= (url[5]||null);
-var host	 	= (url[4]||null);
-var storage		= process.env.DATABASE_STORAGE;
-
 // Cargar Modelo ORM
 var Sequelize = require('sequelize');
+
+// Postgres DATABASE_URL = postgres://user:passwd@host:port/database
+// SQLite   DATABASE_URL = sqlite://:@:/
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name		= (url[6] || null);
+var user		= (url[2] || null);
+var pwd 		= (url[3] || null);
+var protocol 	= (url[1] || null);
+var dialect 	= (url[1] || null);
+var port 		= (url[5] || null);
+var host	 	= (url[4] || null);
+var storage		= process.env.DATABASE_STORAGE;
 
 // Usar BBDD SQLite
 var sequelize = new Sequelize(DB_name, user, pwd,
@@ -28,19 +28,22 @@ var sequelize = new Sequelize(DB_name, user, pwd,
 
 // Importar definicion de la tabla Quiz
 var quiz_path = path.join(__dirname, 'quiz');
-var Quiz = sequelize.Import(quiz_path);
+var Quiz = sequelize.import(quiz_path);
 
 exports.Quiz = Quiz;  // exportar tabla Quiz
 
 // sequelize.sync() crea e inicializa tabla de preguntas en DB
 sequelize.sync().then(function() {
-	// success(..) ejecuta el manejador una vez creada la tabla
-	Quiz.count().success(function(count){
+	// then(..) ejecuta el manejador una vez creada la tabla
+	Quiz.count().then(function(count){
 		if (count === 0) { // la tabla se inicializa solo si está vacia
 			Quiz.create({ pregunta: 'Capital de Italia',
 						  respuesta: 'Roma'
+						});
+			Quiz.create({ pregunta: 'Capital de Portugal',
+						  respuesta: 'Lisboa'
 						})
-			.success(function(){console.log('Base de datos inicializada')});
+			.then(function(){console.log('Base de datos inicializada')});
 		};
 	});
 });
